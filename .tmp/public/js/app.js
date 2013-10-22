439e163bc61b76aebb29a072d6a942d425b482e3
surgesoft <surgesoft@gmail.com>
2013-10-22 22:41:25 +0800

    featued slider

--- .tmp/public/js/app.js
+++ .tmp/public/js/app.js
@@ -1,1 +1,28 @@
  $(document).foundation();
+
+ $(function(){
+
+ 	var featured_index = 0;
+ 	var length =  $('.feature-gallery li').length;
+ 	
+ 	$('.arrow-left').click(function() {
+ 		if (featured_index === 0) {
+ 			featured_index = length -1;
+ 		} else {
+ 			featured_index -= 1;
+ 		}
+ 		$('.feature-gallery').animate({'left':featured_index * -360 });
+ 	});
+
+ 	$('.arrow-right').click(function() {
+ 		if (featured_index === length -1) {
+ 			featured_index = 0;
+ 		} else {
+ 			featured_index += 1;
+ 		}
+ 		$('.feature-gallery').animate({'left':featured_index * -360 });
+ 	});
+
+
+
+ });
